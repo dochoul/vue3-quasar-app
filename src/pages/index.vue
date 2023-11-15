05ffcf5f15c0c5f2c5e27170a6dadba96782382c
dochoul <dochoul@gmail.com>
2023-11-15 17:27:32 +0900

    커뮤니티 목록 UI : "컴포넌트 배치 및 정렬 친해지기!"

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,20 +2,35 @@
   <q-page padding>
     <div class="text-h4">커뮤니티 목록</div>
     <section class="q-gutter-y-smq-mt-lg">
-      <q-card v-for="id in 100" :key="id" @click="$event => goPostDetails(id)">
-        <g-card-section>{{ id }}번 게시글</g-card-section>
-      </q-card>
+      <PostList :items="posts" />
     </section>
   </q-page>
 </template>
 
 <script setup>
+import PostList from 'src/components/apps/post/PostList.vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-const goPostDetails = id => {
-  router.push(`/posts/${id}`);
-};
+
+const posts = Array.from(Array(20), (_, index) => ({
+  id: index,
+  content:
+    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio aliquam officia pariatur culpa delectus, in earum ex velit perspiciatis qui fugit laudantium ducimus nisi natus! Quaerat placeat alias ipsa recusandae?',
+  readCount: 1,
+  commentCount1: 2,
+  likeCount: 3,
+  bookmarkCount: 4,
+  tags: ['html', 'css', 'javascript'],
+  uid: 'uid',
+  category: '카테고리',
+  default: () => {},
+  title: 'Vue3 Firebase 강의 ' + index,
+}));
+
+// const goPostDetails = id => {
+//   router.push(`/posts/${id}`);
+// };
 </script>
 
 <style lang="scss" scoped></style>
